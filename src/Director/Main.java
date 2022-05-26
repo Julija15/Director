2418f47927f181e6ab68d606030d01cbab35c213
Julija15 <1771808@mail.ru>
2022-05-26 03:15:30 +0300

    1234567

--- src/Director/Main.java
+++ src/Director/Main.java
@@ -1,11 +1,10 @@
 package Director;
 public class Main {
     public static void main(String[] args) {
-        String i;
     DirectorClass directorClass = new DirectorClass();
     RabochiyClass rabochiyClass = new RabochiyClass();
     BuchgalterClass buchgalterClass = new BuchgalterClass();
-    Doljnosti doljnosti = new Doljnosti();
+
 
         directorClass.Doljnosty();
         rabochiyClass.Doljnosty();
